b1cbe550135ac3986d71a59c157921089283898e
Ismael Calle <iscam2216@gmail.com>
2025-12-21 15:38:38 -0600

    release v0.3.2

--- src/modules/jobs/base.ts
+++ src/modules/jobs/base.ts
@@ -1,10 +1,7 @@
-import logger from '@zanix/logger'
-import { ProgramModule } from '@zanix/server'
 import type { BaseJob, JobProcess } from 'typings/jobs.ts'
-import type { Execution, SubscriberMetadata } from 'typings/queues.ts'
+import type { Execution } from 'typings/queues.ts'
 
 import { registerTask } from 'utils/tasks.ts'
-import { SUBSCRIBERS_METADATA_KEY } from 'utils/constants.ts'
 
 // deno-lint-ignore no-explicit-any
 export const getRegisterBaseJobData = <J extends JobProcess<any> & BaseJob>(
@@ -14,37 +11,10 @@ export const getRegisterBaseJobData = <J extends JobProcess<any> & BaseJob>(
 
   const queue = processingQueue ? `zanix.worker.${processingQueue}` : customQueue
 
-  setTimeout(() => {
-    const inProcessQueues =
-      ProgramModule.registry.get<SubscriberMetadata[]>(SUBSCRIBERS_METADATA_KEY['main-process']) ||
-      []
-    const outOfProcessQueues = ProgramModule.registry.get<SubscriberMetadata[]>(
-      SUBSCRIBERS_METADATA_KEY['extra-process'],
-    ) ||
-      []
+  const execution: Execution = Deno.env.get('ZANIX_WORKER_EXECUTION') as Execution || 'main-process'
 
-    const exists = inProcessQueues.find((q) => q[0] === queue) ||
-      outOfProcessQueues.find((q) => q[0] === queue)
-
-    if (!exists) {
-      logger.error(
-        `The specified queue '${queue}' does not exist or is not properly configured.`,
-        {
-          meta: {
-            jobName: name,
-            queue,
-            hint:
-              'Ensure the queue is registered in the system and that the configuration matches the expected format.',
-          },
-        },
-      )
-    }
-  }, 500)
-
-  let execution: Execution = 'main-process'
   if (handler) {
     job.args = { $args: job.args, $taskId: registerTask(name, handler) }
-    execution = 'extra-process'
   }
 
   return {
